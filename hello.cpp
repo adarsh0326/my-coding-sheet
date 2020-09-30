5c8090e75afed324b46ac3e7c1b693e44aa3b7dd
Aman Srivastava <56104541+amansrivastava905@users.noreply.github.com>
2020-10-01 01:32:41 +0530

    Update hello.cpp

--- hello.cpp
+++ hello.cpp
@@ -1,9 +1,12 @@
 //find the errors in this code
-#include <bits/stdc>
-using namespace std
+#include <bits/stdc++.h>
+using namespace std;
 
 int main()
 {
   cout<<"Hello world";
   return 0;
 }
+
+//added corrections in line 2,3
+//aman
