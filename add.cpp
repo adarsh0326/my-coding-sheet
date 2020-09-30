b9f2a713cb142638e9d1ecbc101766226ebde789
Aman Srivastava <56104541+amansrivastava905@users.noreply.github.com>
2020-10-01 01:30:24 +0530

    Update add.cpp

--- add.cpp
+++ add.cpp
@@ -1,2 +1,14 @@
 //write a code in any language to add two numbers
 //write your name in comment like this and then your code below that comment
+
+//aman srivastava
+//so, now you all know how to clone a repo and re upload it. now let me create a pr and you try to accept or decline it by analysing it.
+
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+  int a,b;
+  cin>>a>>b;
+  cout<<a+b;
+}
